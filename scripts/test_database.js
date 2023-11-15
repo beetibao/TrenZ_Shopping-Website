6ded5617138a00915c1f4c4cc6d4865004ddae76
beetibao <20521107@gm.uit.edu.vn>
2023-11-15 09:40:19 +0700

    test database

--- scripts/test_database.js
+++ scripts/test_database.js
@@ -4,17 +4,25 @@ const config = {
     user: 'trenz',
     password: 'nhom1_2023',
     server: 'trenz.database.windows.net',
-    database: 'database'
+    database: 'database',
+    options: {
+        encrypt: true, // for azure
+        trustServerCertificate: false // change to true for local dev / self-signed certs
+      }
 };
 
 async function getProduct(){
     let pool = await mssql.connect(config);
-    let product = await pool.request().query("SELECT * from product")
-    return product.recordset;
+    let order = await pool.request().query('SELECT * from [dbo].[order]')
+    return order.recordset;
 }
+
 
 (async () => {
     getProduct().then((result) => {
-        console.log(result);
+        console.log(result[2]);
+        //console.log(result[2].detail);
+        //const b = JSON.parse(result[2].detail);
+        //console.log(b);
     });
 })();

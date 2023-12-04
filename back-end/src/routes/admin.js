d76cad470979eef1cc7c4503d5a07f33de362350
LeVanKhoa <20521467@gm.uit.edu.vn>
2023-12-04 11:37:08 +0700

    show icon user header

--- back-end/src/routes/admin.js
+++ back-end/src/routes/admin.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const AdminController = require("../app/controllers/adminController");
-const methodOverride = require('method-override')
+/*const methodOverride = require('method-override')
 router.use(express.urlencoded({ extended: true }));
-router.use(methodOverride('_method'));
+router.use(methodOverride('_method'));*/
 
 router.use("/admin_statusOrder", AdminController.index1);
 router.use("/admin_listProduct", AdminController.index);
